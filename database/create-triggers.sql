54834ac7e05aa28d8119fcb8212269d26fe9546b
Mariano Caminos <136193679+marian-vg@users.noreply.github.com>
2026-05-31 17:29:53 -0300

    minor-feat: created a .sql file where all the triggers are gonna be write

---
+++ database/create-triggers.sql
@@ -0,0 +1,9 @@
+-- ============================================================================
+-- TRIGGERS - SMART CITY
+-- ============================================================================
+
+-- Este archivo sera para ir agregando los triggers a la base de datos.
+-- Pongan sus triggers aca sin temor, luego el encargado de resolver los merge en caso de que haya conflictos 
+-- (poco probable) se las arreglara. (osea Mariano, yo)
+--
+--
