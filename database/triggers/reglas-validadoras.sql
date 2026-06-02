3855ed937ab34d7ceffea73f5d27b2e41dcd3479
Mariano Caminos <136193679+marian-vg@users.noreply.github.com>
2026-06-02 16:13:12 -0300

    feat: unification of rules. [R8 + R10] and [R9 + R11]

--- database/triggers/reglas-validadoras.sql
+++ database/triggers/reglas-validadoras.sql
@@ -5,28 +5,50 @@
 -- NOTA: en la carpeta /documentacion deje apuntes sobre las variables especiales de pl/pgsql 
 -- y tambien extras como el marcador de posicion "%"
 -- "%" -> marcador de posicion para dar formato dinamico y mas friendly a los mensajes de error.
-
+--
+-- Este script unifica las reglas validadoras de negocio del Smart City por tabla para optimizar 
+-- el rendimiento y evitar redundancia de consultas en la base de datos.
+--
+-- =============================================================================================================
 
 -- =============================================================================================================
--- R8. Validación de disponibilidad de recursos
--- No se podrá asignar un recurso que ya esté ocupado.
---
--- TOC -> BEFORE.
--- Granularidad -> FOR EACH ROW.
--- FOR UPDATE -> evitar problemas de concurrencia mediante el bloqueo de la fila.
--- TG_OP -> variable especial que nos da PL/pgsql para ver la accion DML que disparo el trigger.
+-- 0. LIMPIEZA DE TRIGGERS Y FUNCIONES INDIVIDUALES ANTERIORES (OBSOLETOS)
+-- =============================================================================================================
+-- DROP TRIGGER IF EXISTS trg_valida_disponibilidad_recurso ON Asignacion;
+-- DROP TRIGGER IF EXISTS trg_valida_zona_recurso ON Asignacion;
+-- DROP TRIGGER IF EXISTS trg_valida_coherencia_estados_incidente ON Incidente;
+-- DROP TRIGGER IF EXISTS trg_valida_duplicacion_incidente ON Incidente;
 
-CREATE OR REPLACE FUNCTION fn_valida_disponibilidad_recurso()
+-- DROP FUNCTION IF EXISTS fn_valida_disponibilidad_recurso();
+-- DROP FUNCTION IF EXISTS fn_valida_zona_recurso();
+-- DROP FUNCTION IF EXISTS fn_valida_coherencia_estados_incidente();
+-- DROP FUNCTION IF EXISTS fn_valida_duplicacion_incidente();
+-- =============================================================================================================
+-- 1. INTEGRIDAD DE ASIGNACIONES (Unifica R8 y R10)
+-- =============================================================================================================
+--
+-- R8. Validación de disponibilidad de recursos: No se podrá asignar un recurso que ya esté ocupado.
+-- R10. Validación de zona del recurso: Un recurso solo podrá asignarse a incidentes dentro de su zona habilitada.
+--
+-- TOC -> BEFORE INSERT OR UPDATE
+-- Granularidad -> FOR EACH ROW
+--
+-- Naming -> fn_valida_registro_asignacion y trg_valida_registro_asignacion
+
+CREATE OR REPLACE FUNCTION fn_valida_registro_asignacion()
 RETURNS TRIGGER AS $$
 DECLARE
     v_estado_nombre VARCHAR(50);
+    v_zona_incidente INT;
 BEGIN
-    -- Si es un UPDATE y no cambia el recurso asignado, no es necesario validar disponibilidad.
+    -- Si es un UPDATE y no cambia el recurso ni el incidente, omitimos validaciones.
     IF TG_OP = 'UPDATE'
-        AND OLD.fk_recurso_id = NEW.fk_recurso_id THEN
+        AND OLD.fk_recurso_id = NEW.fk_recurso_id
+        AND OLD.fk_incidente_id = NEW.fk_incidente_id THEN
             RETURN NEW;
     END IF;
 
+    -- Carga de contexto: Obtenemos el estado del recurso y la zona del incidente en una sola lectura
     SELECT EstadoRecurso.nombre 
     INTO v_estado_nombre
     FROM Recurso
@@ -34,66 +56,126 @@ BEGIN
     WHERE Recurso.id_recurso = NEW.fk_recurso_id
     FOR UPDATE;
 
-    -- Validamos la disponibilidad.
+    SELECT Incidente.fk_zona_id 
+    INTO v_zona_incidente
+    FROM Incidente
+    WHERE Incidente.id_incidente = NEW.fk_incidente_id;
+
+    -- -------------------------------------------------------------------------
+    -- R8. VALIDACIÓN DE DISPONIBILIDAD
+    -- -------------------------------------------------------------------------
+    -- Validamos que el estado del recurso sea 'Disponible'
     IF v_estado_nombre IS DISTINCT FROM 'Disponible' THEN
-        RAISE EXCEPTION 'No se puede asignar el recurso % porque no se encuentra disponible.', 
-            NEW.fk_recurso_id;
+        RAISE EXCEPTION 'No se puede asignar el recurso % porque no se encuentra disponible (Estado: %).', 
+            NEW.fk_recurso_id, v_estado_nombre;
     END IF;
 
-    -- Validamos que no este en una asignacion activa.
+    -- Validamos que no cuente con una asignación activa (finalización nula)
     IF EXISTS (
         SELECT 1 
         FROM Asignacion 
-        WHERE fk_recurso_id = NEW.fk_recurso_id 
-          AND timestamp_finalizacion IS NULL
-          AND id_asignacion IS DISTINCT FROM NEW.id_asignacion
+        WHERE Asignacion.fk_recurso_id = NEW.fk_recurso_id 
+          AND Asignacion.timestamp_finalizacion IS NULL
+          AND Asignacion.id_asignacion IS DISTINCT FROM NEW.id_asignacion
     ) THEN
         RAISE EXCEPTION 'No se puede asignar el recurso % porque ya cuenta con una asignación activa en curso.', 
             NEW.fk_recurso_id;
+    END IF;
+
+    -- -------------------------------------------------------------------------
+    -- R10. VALIDACIÓN DE ZONA
+    -- -------------------------------------------------------------------------
+    IF COALESCE(current_setting('my.bypass_zona', true), '') IS DISTINCT FROM '1' THEN
+        IF NOT EXISTS (
+            SELECT 1 
+            FROM ZonaRecurso
+            WHERE ZonaRecurso.id_recurso = NEW.fk_recurso_id
+              AND ZonaRecurso.id_zona = v_zona_incidente
+        ) THEN
+            RAISE EXCEPTION 'El recurso % no está habilitado para operar en la zona del incidente (Zona %).',
+                NEW.fk_recurso_id, v_zona_incidente;
+        END IF;
     END IF;
 
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
-DROP TRIGGER IF EXISTS trg_valida_disponibilidad_recurso ON Asignacion;
-CREATE TRIGGER trg_valida_disponibilidad_recurso
-BEFORE INSERT OR UPDATE OF fk_recurso_id ON Asignacion
+DROP TRIGGER IF EXISTS trg_valida_registro_asignacion ON Asignacion;
+CREATE TRIGGER trg_valida_registro_asignacion
+BEFORE INSERT OR UPDATE OF fk_recurso_id, fk_incidente_id ON Asignacion
 FOR EACH ROW
-EXECUTE FUNCTION fn_valida_disponibilidad_recurso();
+EXECUTE FUNCTION fn_valida_registro_asignacion();
 
 -- =============================================================================================================
-
--- R9. Validación de coherencia de estados
--- Se deberán evitar cambios de estado inválidos.
+-- 2. CONSISTENCIA DE INCIDENTES (Unifica R9 y R11)
+-- =============================================================================================================
 --
--- TOC -> BEFORE
+-- R9. Validación de coherencia de estados: Evitar cambios de estado inválidos (ej. Pendiente -> Resuelto).
+-- R11. Validación de duplicación de incidentes: Evitar registrar incidentes duplicados en un período corto.
+--
+-- TOC -> BEFORE INSERT OR UPDATE
 -- Granularidad -> FOR EACH ROW
 --
--- Las dos primeras condicionales son para determinar los cambios mas criticos de transiciones
--- y para retornar un mensaje apropiado a las mismas.
+-- Naming -> fn_valida_registro_incidente y trg_valida_registro_incidente
 
-CREATE OR REPLACE FUNCTION fn_valida_coherencia_estados_incidente()
+CREATE OR REPLACE FUNCTION fn_valida_registro_incidente()
 RETURNS TRIGGER AS $$
 DECLARE
+    v_minutos_duplicado NUMERIC;
     v_estado_old VARCHAR(50);
     v_estado_new VARCHAR(50);
 BEGIN
-    IF OLD.fk_estado_incidente_id IS DISTINCT FROM NEW.fk_estado_incidente_id THEN
-        
+    -- -------------------------------------------------------------------------
+    -- R11. VALIDACIÓN DE DUPLICACIÓN (Solo en INSERT o cambio de zona/tipo en UPDATE)
+    -- -------------------------------------------------------------------------
+    IF TG_OP = 'INSERT' OR (
+        TG_OP = 'UPDATE' AND (
+            OLD.fk_zona_id IS DISTINCT FROM NEW.fk_zona_id OR 
+            OLD.fk_tipo_incidente_id IS DISTINCT FROM NEW.fk_tipo_incidente_id
+        )
+    ) THEN
+        -- Buscamos el parámetro de minutos de duplicación
+        SELECT ParametrosSistema.numero
+        INTO v_minutos_duplicado
+        FROM ParametrosSistema
+        WHERE ParametrosSistema.nombre_parametro = 'MINUTOS_DUPLICADO_INCIDENTE';
+
+        v_minutos_duplicado := COALESCE(v_minutos_duplicado, 10);
+
+        -- Validar duplicidad
+        IF EXISTS (
+            SELECT 1
+            FROM Incidente
+            JOIN EstadoIncidente ON Incidente.fk_estado_incidente_id = EstadoIncidente.id_estado_incidente
+            WHERE Incidente.fk_zona_id = NEW.fk_zona_id
+              AND Incidente.fk_tipo_incidente_id = NEW.fk_tipo_incidente_id
+              AND EstadoIncidente.nombre IS DISTINCT FROM 'Cancelado'
+              AND Incidente.fecha_hora_registro >= CURRENT_TIMESTAMP - (v_minutos_duplicado * INTERVAL '1 minute')
+              AND (TG_OP = 'INSERT' OR Incidente.id_incidente IS DISTINCT FROM NEW.id_incidente)
+        ) THEN
+            RAISE EXCEPTION 'Ya existe un incidente activo del mismo tipo en la zona dentro de los últimos % minutos.',
+                v_minutos_duplicado;
+        END IF;
+    END IF;
+
+    -- -------------------------------------------------------------------------
+    -- R9. VALIDACIÓN DE COHERENCIA DE ESTADOS (Solo en UPDATE cuando cambia el estado)
+    -- -------------------------------------------------------------------------
+    IF TG_OP = 'UPDATE' AND OLD.fk_estado_incidente_id IS DISTINCT FROM NEW.fk_estado_incidente_id THEN
         -- Obtener los nombres de los estados involucrados
-        SELECT nombre 
+        SELECT EstadoIncidente.nombre 
         INTO v_estado_old 
         FROM EstadoIncidente 
-        WHERE id_estado_incidente = OLD.fk_estado_incidente_id;
+        WHERE EstadoIncidente.id_estado_incidente = OLD.fk_estado_incidente_id;
 
-        SELECT nombre 
+        SELECT EstadoIncidente.nombre 
         INTO v_estado_new 
         FROM EstadoIncidente 
-        WHERE id_estado_incidente = NEW.fk_estado_incidente_id;
+        WHERE EstadoIncidente.id_estado_incidente = NEW.fk_estado_incidente_id;
 
         -- No se puede modificar el estado si ya está 'Resuelto' o 'Cancelado'.
-        IF v_estado_old = 'Resuelto' OR v_estado_old = 'Cancelado' THEN
+        IF v_estado_old IN ('Resuelto', 'Cancelado') THEN
             RAISE EXCEPTION 'No se permite modificar el estado de un incidente que ya ha sido Resuelto o Cancelado.';
         END IF;
         
@@ -102,14 +184,11 @@ BEGIN
             RAISE EXCEPTION 'Transición de estado inválida: No se puede pasar de Pendiente directamente a Resuelto.';
         END IF;
 
-
-        -- Validacion mediante una whitelist sobre el resto de transiciones de estados.
-
+        -- Whitelist de transiciones válidas:
         -- Pendiente -> En proceso, Escalado, En espera, Cancelado
         -- En proceso -> Resuelto, Escalado, Cancelado
         -- Escalado -> En proceso, Resuelto, Cancelado
         -- En espera -> Pendiente, En proceso, Cancelado
-
         IF NOT (
             (v_estado_old = 'Pendiente' AND v_estado_new IN ('En proceso', 'Escalado', 'En espera', 'Cancelado')) 
             OR
@@ -122,109 +201,14 @@ BEGIN
             RAISE EXCEPTION 'Transición de estado de incidente no permitida por regla de negocio: % -> %', 
                 v_estado_old, v_estado_new;
         END IF;
-
     END IF;
 
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
-DROP TRIGGER IF EXISTS trg_valida_coherencia_estados_incidente ON Incidente;
-CREATE TRIGGER trg_valida_coherencia_estados_incidente
-BEFORE UPDATE OF fk_estado_incidente_id ON Incidente
+DROP TRIGGER IF EXISTS trg_valida_registro_incidente ON Incidente;
+CREATE TRIGGER trg_valida_registro_incidente
+BEFORE INSERT OR UPDATE ON Incidente
 FOR EACH ROW
-EXECUTE FUNCTION fn_valida_coherencia_estados_incidente();
-
--- =============================================================================================================
-
--- R10. Validación de zona del recurso
--- Un recurso solo podrá asignarse a incidentes dentro de su zona habilitada.
---
--- TOC -> BEFORE
--- Granularidad -> FOR EACH ROW
-
-CREATE OR REPLACE FUNCTION fn_valida_zona_recurso()
-RETURNS TRIGGER AS $$
-DECLARE
-    v_zona_incidente INT;
-BEGIN
-    -- Si es un UPDATE y no cambia ni el recurso ni el incidente, no es necesario validar.
-    IF TG_OP = 'UPDATE'
-        AND OLD.fk_recurso_id = NEW.fk_recurso_id
-        AND OLD.fk_incidente_id = NEW.fk_incidente_id THEN
-            RETURN NEW;
-    END IF;
-
-    -- Obtenemos la zona en la que ocurrió el incidente.
-    SELECT Incidente.fk_zona_id
-    INTO v_zona_incidente
-    FROM Incidente
-    WHERE Incidente.id_incidente = NEW.fk_incidente_id;
-
-    -- Corroboramos si se esta realizando un rebalanceo de emergencia. 
-    -- (coalesce retorna '' en caso que el setting no exista, caso contrario se valida la emergencia y asignamos el recurso)
-    IF COALESCE(current_setting('my.bypass_zona', true), '') = '1' THEN
-        RETURN NEW;
-    END IF;
-
-    -- Validamos si el recurso está habilitado para operar en dicha zona.
-    IF NOT EXISTS (
-        SELECT 1
-        FROM ZonaRecurso
-        WHERE ZonaRecurso.id_recurso = NEW.fk_recurso_id
-          AND ZonaRecurso.id_zona = v_zona_incidente
-    ) THEN
-        RAISE EXCEPTION 'El recurso % no está habilitado para operar en la zona del incidente',
-            NEW.fk_recurso_id;
-    END IF;
-
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
-DROP TRIGGER IF EXISTS trg_valida_zona_recurso ON Asignacion;
-CREATE TRIGGER trg_valida_zona_recurso
-BEFORE INSERT OR UPDATE OF fk_recurso_id, fk_incidente_id ON Asignacion
-FOR EACH ROW
-EXECUTE FUNCTION fn_valida_zona_recurso();
-
--- =============================================================================================================
-
--- R11. Validación de duplicación de incidentes
--- Se deberá evitar registrar incidentes duplicados en un corto período (misma zona y tipo).
-
-CREATE OR REPLACE FUNCTION fn_valida_duplicacion_incidente()
-RETURNS TRIGGER AS $$
-DECLARE
-    v_minutos_duplicado NUMERIC;
-BEGIN
-    -- Buscamos el parametro de los minutos de duplicacion en la tabla de parámetros de sistema.
-    SELECT ParametrosSistema.numero
-    INTO v_minutos_duplicado
-    FROM ParametrosSistema
-    WHERE ParametrosSistema.nombre_parametro = 'MINUTOS_DUPLICADO_INCIDENTE';
-
-    -- Buscamos si existe algún incidente similar registrado recientemente.
-    IF EXISTS (
-        SELECT 1
-        FROM Incidente
-        JOIN EstadoIncidente ON Incidente.fk_estado_incidente_id = EstadoIncidente.id_estado_incidente
-        WHERE Incidente.fk_zona_id = NEW.fk_zona_id
-          AND Incidente.fk_tipo_incidente_id = NEW.fk_tipo_incidente_id
-          AND EstadoIncidente.nombre IS DISTINCT FROM 'Cancelado'
-          AND Incidente.fecha_hora_registro >= CURRENT_TIMESTAMP - (v_minutos_duplicado * INTERVAL '1 minute')
-          AND (TG_OP = 'INSERT' OR Incidente.id_incidente IS DISTINCT FROM NEW.id_incidente)
-    ) THEN
-        RAISE EXCEPTION 'Ya existe un incidente activo del mismo tipo en la zona dentro de los últimos % minutos.',
-            v_minutos_duplicado;
-    END IF;
-
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
-DROP TRIGGER IF EXISTS trg_valida_duplicacion_incidente ON Incidente;
-CREATE TRIGGER trg_valida_duplicacion_incidente
-BEFORE INSERT OR UPDATE OF fk_zona_id, fk_tipo_incidente_id ON Incidente
-FOR EACH ROW
-EXECUTE FUNCTION fn_valida_duplicacion_incidente();
+EXECUTE FUNCTION fn_valida_registro_incidente();
